95150d6422ce001abd026360f9ff32084358dfa1
Cjr <carloscjr70@gmail.com>
2023-02-01 05:51:07 -0300

    alterações realizadas com sucesso.

--- js/main.js
+++ js/main.js
@@ -11,17 +11,27 @@ form.addEventListener('submit', function(e){
     let validaForm = false
     e.preventDefault();
 
-validaForm = validaNumero(numA.value, numB.value)
+const notaA = document.getElementById('numero-a').value;
+const notaB = document.getElementById('numero-b').value;
+const mensagemSucesso =`O número ${numB.value} é maior que o ${numA.value}.`
+const mensagemSucesso2 = `O numero ${numA.value} é maior que o ${numB.value} `
 
-if(validaForm){
+if(notaA < notaB){
 
-    alert('Certo')
+    const containerMensagemSucesso = document.querySelector('.mensagem-sucesso')
+    containerMensagemSucesso.innerHTML = mensagemSucesso 
+    containerMensagemSucesso.style.display = 'block'
 
-    numA.value = ''
-    numB.value = ''
+    document.getElementById('numero-a').value=''
+    document.getElementById('numero-b').value=''
 
 }else{
-    alert('Errado')
+    const containerMensagemSucesso2 = document.querySelector('.mensagem-sucesso')
+    containerMensagemSucesso2.innerHTML = mensagemSucesso2
+    containerMensagemSucesso2.style.display = 'block'
+
+    document.getElementById('numero-a').value=''
+    document.getElementById('numero-b').value=''
 }
 
 })
